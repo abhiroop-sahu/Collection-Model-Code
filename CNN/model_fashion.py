678edd29db5161e373525431fc4977516e32d69c
Abhiroop Sahu <abhsahu000@gmail.com>
2025-03-03 10:11:05 -0600

    Update model_fashion.py

--- CNN/model_fashion.py
+++ CNN/model_fashion.py
@@ -1,1 +1,36 @@
+import torch.nn as nn
+import torch.nn.functional as F
 
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=3)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.conv3 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=2)
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(in_features=4096, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=1024)
+        self.fc3 = nn.Linear(in_features=1024, out_features=10)
+
+
+    def forward(self, x):
+      x = F.relu(self.conv1(x))
+      x = self.pool1(x)
+      x = F.relu(self.conv2(x))
+      x = self.pool2(x)
+      x = F.relu(self.conv3(x))
+      x = self.pool3(x)
+      x = self.flatten(x)
+      x = F.relu(self.fc1(x))
+      x = F.relu(self.fc2(x))
+      x = self.fc3(x)
+
+      return x
