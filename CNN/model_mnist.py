950656c05a6f1a551ee5ec0723f1e4ec4fe84cce
Abhiroop Sahu <abhsahu000@gmail.com>
2025-03-03 10:04:19 -0600

    Update model_mnist.py

--- CNN/model_mnist.py
+++ CNN/model_mnist.py
@@ -1,1 +1,20 @@
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
 
+        self.conv1 = nn.Conv2d(1, 20, 5)
+        self.conv2 = nn.Conv2d(20, 40, 5)
+        self.fc1 = nn.Linear(40*4*4, 100)
+        self.fc2 = nn.Linear(100, 50)
+        self.fc3 = nn.Linear(50, 10)
+
+    def forward(self, x): # x is the input
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, (2, 2))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, (2, 2))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x)) # the output
+        return x
