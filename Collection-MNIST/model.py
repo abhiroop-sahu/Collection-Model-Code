9ca4ceef90b460fbb77bd95b487ba2581c2f4c1d
Abhiroop Sahu <abhsahu000@gmail.com>
2025-03-03 10:15:56 -0600

    Update model.py

--- Collection-MNIST/model.py
+++ Collection-MNIST/model.py
@@ -1,1 +1,14 @@
+class CollectionNet(nn.Module):
+  def __init__(self):
+    super(CollectionNet, self).__init__()
 
+    self.conv = nn.Conv2d(1, 10, 6)
+    self.pl1 = nn.Linear(10*9*9, 50)
+    self.pl2 = nn.Linear(50, 10)
+
+  def forward(self, x):
+    x = F.relu(self.conv(x))
+    x = torch.flatten(x, 1)
+    x = F.relu(self.pl1(x))
+    x = F.relu(self.pl2(x)) # the output
+    return x
