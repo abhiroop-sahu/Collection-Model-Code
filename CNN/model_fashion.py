7d0f377017ef3448c90e66eb7175bb1a39a3a2f1
Abhiroop Sahu <abhsahu000@gmail.com>
2025-03-05 09:19:51 -0600

    Update model_fashion.py

--- CNN/model_fashion.py
+++ CNN/model_fashion.py
@@ -1,21 +1,17 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class NeuralNet(nn.Module):
+class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=3)
         self.pool1 = nn.MaxPool2d(2, 2)
-
         self.conv2 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3)
         self.pool2 = nn.MaxPool2d(2, 2)
-
         self.conv3 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=2)
         self.pool3 = nn.MaxPool2d(2, 2)
-
         self.flatten = nn.Flatten()
-
         self.fc1 = nn.Linear(in_features=4096, out_features=1024)
         self.fc2 = nn.Linear(in_features=1024, out_features=1024)
         self.fc3 = nn.Linear(in_features=1024, out_features=10)
@@ -34,3 +30,7 @@ class NeuralNet(nn.Module):
       x = self.fc3(x)
 
       return x
+
+model_reg = ConvNet().to(device)
+loss_fn_reg = nn.CrossEntropyLoss()
+optimizer_reg = optim.Adam(model_reg.parameters(), lr=0.0001)
